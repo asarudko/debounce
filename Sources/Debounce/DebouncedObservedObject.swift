8557d236f2f1e84b85132e77cfef99026b301643
Anton Sarudko <anton.sarudko@zuehlke.com>
2023-02-24 11:34:18 +0100

    Inital commit

---
+++ Sources/Debounce/DebouncedObservedObject.swift
@@ -0,0 +1,25 @@
+//
+//  DebouncedObservedObject.swift
+//  
+//
+//  Created by Anton Sarudko on 24.02.23.
+//
+
+import Combine
+import Foundation
+
+@propertyWrapper
+class DebouncedObservedObject<Wrapped: ObservableObject>: ObservableObject {
+    var wrappedValue: Wrapped
+    private var subscription: AnyCancellable?
+    
+    public init(wrappedValue: Wrapped, delay: Double = 1) {
+        self.wrappedValue = wrappedValue
+        self.subscription = wrappedValue
+            .objectWillChange
+            .debounce(for: .seconds(delay), scheduler: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.objectWillChange.send()
+            }
+    }
+}
---
+++ Sources/Debounce/Debouncer.swift
@@ -0,0 +1,27 @@
+//
+//  Debouncer.swift
+//  
+//
+//  Created by Anton Sarudko on 24.02.23.
+//
+
+import Combine
+import Foundation
+
+public class Debouncer<T>: ObservableObject {
+    
+    // MARK: - Properties
+    @Published public var input: T
+    @Published private(set) var output: T
+    private var debounce: AnyCancellable?
+    
+    public init(initialValue: T, delay: Double = 1) {
+        self.input = initialValue
+        self.output = initialValue
+        self.debounce = $input
+            .debounce(for: .seconds(delay), scheduler: DispatchQueue.main)
+            .sink { [weak self] in
+                self?.output = $0
+            }
+    }
+}
